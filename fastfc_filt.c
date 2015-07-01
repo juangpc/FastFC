3b9fba02a9be3644f9d0498d5fada8fea448f86f
Juan <juangpc@gmail.com>
2015-07-01 15:03:01 +0200

    better this way of calc circular padding.
    this way circular padding can be used with many sensors in mind.

--- fastfc_filt.c
+++ fastfc_filt.c
@@ -28,11 +28,9 @@
 #include <mex.h>
 #include <fftw3.h>
 
-
 void mexFunction(int nlhs,mxArray *plhs[],int nrhs,const mxArray *prhs[])
 {
-    
-    mwSize i,aux_i;
+    mwSize i;
     float *b,*x;
     double *x_d,*b_d,*y;
     fftwf_complex *fft_b,*fft_x;
@@ -49,97 +47,26 @@ void mexFunction(int nlhs,mxArray *plhs[],int nrhs,const mxArray *prhs[])
     b_d=(double*)mxGetPr(prhs[0]);    
     x_d=(double*)mxGetPr(prhs[1]);
     
-    b=(float*)malloc(l_p*sizeof(float));
-    x=(float*)malloc(l_p*sizeof(float));
+    b=(float*)mxMalloc(l_p*sizeof(float));
+    x=(float*)mxMalloc(l_p*sizeof(float));
     fft_x=(fftwf_complex*)fftwf_malloc(l_p*sizeof(fftwf_complex));
     fft_b=(fftwf_complex*)fftwf_malloc(l_p*sizeof(fftwf_complex));
     
-    if(mode==2)
+    if(mode==3)
     {
-        p_r2c=fftwf_plan_dft_r2c_1d(l_p,b,fft_b,FFTW_EXHAUSTIVE);
-        p_c2r=fftwf_plan_dft_c2r_1d(l_p,fft_b,b,FFTW_EXHAUSTIVE);
+        p_r2c=fftwf_plan_dft_r2c_1d((int)l_p,b,fft_b,FFTW_EXHAUSTIVE);
+        p_c2r=fftwf_plan_dft_c2r_1d((int)l_p,fft_b,b,FFTW_EXHAUSTIVE);
     }
+    else if(mode==2)
+    {
+        p_r2c=fftwf_plan_dft_r2c_1d((int)l_p,b,fft_b,FFTW_MEASURE);
+        p_c2r=fftwf_plan_dft_c2r_1d((int)l_p,fft_b,b,FFTW_MEASURE);
+    }
+
     else
     {
-        p_r2c=fftwf_plan_dft_r2c_1d(l_p,b,fft_b,FFTW_ESTIMATE);
-        p_c2r=fftwf_plan_dft_c2r_1d(l_p,fft_b,b,FFTW_ESTIMATE);
-    }                                           
-
-    //copy b with normal padding
-    for(i=0;i<l_b;i++)
-        b[i]=(float)b_d[i];
-    for(;i<l_p;i++)
-        b[i]=(float)0.;
-    //execute fft of b
-    fftwf_execute_dft_r2c(p_r2c,b,fft_b);
-    
-    //copy sensor with circular padding
-    for(i=0;i<l_b-2;i++)
-        x[i]=(2.*(float)x_d[0])-(float)x_d[l_b-2-i];
-    for(;i<l_p-l_b;i++)
-        x[i]=(float)x_d[i-l_b+2];
-    for(;i<l_p;i++)
-        x[i]=(2.*x[l_p-l_b-1])-x[2*(l_p-l_b-1)-i];
-    
-    //calc fft of sensor
-    fftwf_execute_dft_r2c(p_r2c,x,fft_x);
-    
-    //calculate filter mask and 
-    //filter in freq domain 
-    for(i=0;i<l_p/2+1;i++)
-    {
-        fft_x[i][0]*=fft_b[i][0]*fft_b[i][0]+fft_b[i][1]*fft_b[i][1];
-        fft_x[i][1]*=fft_b[i][0]*fft_b[i][0]+fft_b[i][1]*fft_b[i][1];
-    }
-    
-    fftwf_execute_dft_c2r(p_c2r,fft_x,x);
-    plhs[0]=mxCreateDoubleMatrix(n_samples,n_sensors,mxREAL);
-    y=(double*)mxGetPr(plhs[0]);
-    
-    aux_i=0;
-    for(i=l_b-2;i<l_p-l_b;i++)
-        y[aux_i++]=(double)(x[i]/l_p);
-
-    mxFree(b);
-    mxFree(x);
-    fftwf_destroy_plan(p_c2r);
-    fftwf_destroy_plan(p_r2c);
-    fftwf_free(fft_b);    
-    fftwf_free(fft_x);
-
-}
-    
-    unsigned int i,aux_i;
-    float *b,*x;
-    double *x_d,*b_d,*y;
-    fftwf_complex *fft_b,*fft_x;
-    fftwf_plan p_r2c,p_c2r;
-    
-    const unsigned int l_b=(unsigned int)mxGetN(prhs[0]);
-    const unsigned int n_samples=(unsigned int)mxGetM(prhs[1]);
-    const unsigned int n_sensors=(unsigned int)mxGetN(prhs[1]);
-    const short int mode=(short int)mxGetScalar(prhs[2]);
-            
-    const unsigned int l_p=n_samples+2*(l_b-1);
-    const int is_even=(l_p%2)?0:1;
-
-    b_d=(double*)mxGetPr(prhs[0]);    
-    x_d=(double*)mxGetPr(prhs[1]);
-    
-    b=(float*)malloc(l_p*sizeof(float));
-    x=(float*)malloc(l_p*sizeof(float));
-    fft_x=(fftwf_complex*)fftwf_malloc(l_p*sizeof(fftwf_complex));
-    fft_b=(fftwf_complex*)fftwf_malloc(l_p*sizeof(fftwf_complex));
-    
-    if(mode==2)
-    {
-        p_r2c=fftwf_plan_dft_r2c_1d(l_p,b,fft_b,FFTW_EXHAUSTIVE);
-        p_c2r=fftwf_plan_dft_c2r_1d(l_p,fft_b,b,FFTW_EXHAUSTIVE);
-    }
-    else
-    {
-        p_r2c=fftwf_plan_dft_r2c_1d(l_p,b,fft_b,FFTW_ESTIMATE);
-        p_c2r=fftwf_plan_dft_c2r_1d(l_p,fft_b,b,FFTW_ESTIMATE);
+        p_r2c=fftwf_plan_dft_r2c_1d((int)l_p,b,fft_b,FFTW_ESTIMATE);
+        p_c2r=fftwf_plan_dft_c2r_1d((int)l_p,fft_b,b,FFTW_ESTIMATE);
     }                                           
 
     //copy b with normal padding
@@ -153,38 +80,39 @@ void mexFunction(int nlhs,mxArray *plhs[],int nrhs,const mxArray *prhs[])
     //calculate filter mask and store in b (as it is Real)
     for(i=0;i<l_p/2+1;i++)
         b[i]=fft_b[i][0]*fft_b[i][0]+fft_b[i][1]*fft_b[i][1];
-    
+	
+	fftwf_free(fft_b);  
+
     //copy sensor with circular padding
     for(i=0;i<l_b-2;i++)
-        x[i]=(2.*(float)x_d[0])-(float)x_d[l_b-2-i];
-    for(i=0;i<n_samples;i++)
-        x[l_b-2+i]=(float)x_d[i];
-    aux_i=1;
+        x[i]=((float)2.*(float)x_d[0])-(float)x_d[l_b-2-i];
+    for(;i<l_p-l_b;i++)
+        x[i]=(float)x_d[i-l_b+2];
     for(;i<l_p;i++)
-        x[l_b-2+i]=(2.*(float)x_d[n_samples-1])-(float)x_d[n_samples-1-aux_i++];
+        x[i]=((float)2.*(float)x[l_p-l_b-1])-(float)x[2*(l_p-l_b-1)-i];
+    
     //calc fft of sensor
     fftwf_execute_dft_r2c(p_r2c,x,fft_x);
-    
-    //filter in freq domain
+	fftwf_destroy_plan(p_r2c);
+
+    //filter in freq domain 
     for(i=0;i<l_p/2+1;i++)
     {
         fft_x[i][0]*=b[i];
         fft_x[i][1]*=b[i];
     }
+    mxFree(b);    
     
-    fftwf_execute_dft_c2r(p_c2r,fft_x,x);
+	fftwf_execute_dft_c2r(p_c2r,fft_x,x);
+    
+	fftwf_destroy_plan(p_c2r);
+    fftwf_free(fft_x);
+
     plhs[0]=mxCreateDoubleMatrix(n_samples,n_sensors,mxREAL);
     y=(double*)mxGetPr(plhs[0]);
     
-    aux_i=0;
-    for(i=l_b-2;i<l_p-l_b;i++)
-        y[aux_i++]=(double)(x[i]/l_p);
+    for(i=0;i<n_samples;i++)
+        y[i]=(double)(x[i+l_b-2]/l_p);
 
-    fftwf_destroy_plan(p_c2r);
-    fftwf_destroy_plan(p_r2c);
-    fftwf_free(fft_b);    
-    fftwf_free(fft_x);
-    free(b);
-    free(x);
-
+    mxFree(x);
 }
