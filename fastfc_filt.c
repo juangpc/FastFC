fd38c419bf28d88d97de55ddac02f14fa609256b
Juan <juangpc@gmail.com>
2015-07-21 16:00:01 +0200

    adding licence to code file

--- fastfc_filt.c
+++ fastfc_filt.c
@@ -1,3 +1,30 @@
+//
+//    Copyright (c) 2014-2015 Juan Garcia-Prieto Cuesta
+//
+//    This file is part of Fast Functional Connectivity (FastFC)
+//
+//    FastFC is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    FastFC is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with FastFC.  If not, see <http://www.gnu.org/licenses/>.
+//
+//    ------------------------------------------ 
+//    Contact:
+//    Juan Garcia-Prieto <juangpc@gmail.com>
+//    ------------------------------------------
+//
+//    Please consider helping by citing our research.
+// 
+//    J. Garcia-Prieto, E. Pereda
+//
 #include <mex.h>
 #include <fftw3.h>
 #include <omp.h>
